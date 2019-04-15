992425bd42101aa5d6bedf486d63775d6712e02e
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-04-15 11:56:41 +0600

    Добавлены файлы проекта

---
+++ RailwayTicket/RailwayTicket/Train.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayTicket
+{
+    public class Train:Entity
+    {
+        public string Number { get; set; }
+        public int Seats { get; set; }
+
+    }
+}
